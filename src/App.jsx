5095866757716a221fa583bbc2d0a89a2caae7b3
Vansh Pathak <hydravansh24@gmail.com>
2026-07-12 03:00:54 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import gsap from 'gsap'
+import { ScrollTrigger, SplitText } from 'gsap/all'
+import { useGSAP } from '@gsap/react'
+
+gsap.registerPlugin(ScrollTrigger, SplitText)
+
+const App = () => {
+
+  return (
+    <div>
+        <h1 className='text-5xl text-indigo-300 cursor-pointer'>Hello</h1>
+    </div>
+  )
+}
+
+export default App
